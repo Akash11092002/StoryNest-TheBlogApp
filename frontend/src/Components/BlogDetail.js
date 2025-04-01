c3f8c91702aa67a8b9b2bc659de9888f4550d08f
AKASH MAURYA <114644236+Akash11092002@users.noreply.github.com>
2025-04-01 15:31:19 +0530

    Update BlogDetail.js

--- frontend/src/Components/BlogDetail.js
+++ frontend/src/Components/BlogDetail.js
@@ -25,7 +25,7 @@ const BlogDetail = () => {
     console.log(id);
 
     const fetchDetails =async ()=>{
-      const res=await axios.get(`http://localhost:5000/api/blog/${id}`).catch(err=> console.log(err));
+      const res=await axios.get(`https://storynest-theblogapp.onrender.com/api/blog/${id}`).catch(err=> console.log(err));
      const data=await res.data;
      return data;
   
@@ -44,7 +44,7 @@ const BlogDetail = () => {
    },[id]);
 
   const sendRequest =async()=>{
-     const res= axios.put(`http://localhost:5000/api/blog/update/${id}`,{
+     const res= axios.put(`https://storynest-theblogapp.onrender.com/api/blog/update/${id}`,{
       title:inputs.title,
       description:inputs.description
      }).catch(err => console.log(err));
