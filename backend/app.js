e03b7cf0671b2a2670432669f511c7d0f2195a44
akash <mauryaakash3965@gmail.com>
2025-04-01 13:03:27 +0530

    Fix mongoose package import issue

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,9 @@ app.use(cors());
 
 app.use("/api/user",router);
 app.use("/api/blog",blogRouter);
-mongoose.connect('mongodb+srv://admin:yna07CYN8AGN29jb@cluster0.nkq1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://newuser:VtcuaWVNW8jaIw5u@cluster0.nkq1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    
+  )
 .then(()=>app.listen(5000))
 .then(()=>console.log("connected to database"))
 .catch((err)=>console.log(err));
